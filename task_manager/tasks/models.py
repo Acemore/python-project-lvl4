132cabe69d46417aa03824dbd750e1556c6feed6
Sergey Martynov <acemore@mail.ru>
2022-05-15 16:20:03 +0300

    Add labels to Task model

--- task_manager/tasks/models.py
+++ task_manager/tasks/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+from task_manager.labels.models import Label
 from task_manager.statuses.models import Status
 from task_manager.users.models import User
 
@@ -27,4 +28,18 @@ class Task(models.Model):
         related_name='status'
     )
 
+    labels = models.ManyToManyField(
+        Label,
+        related_name='labels',
+        blank=True,
+        through='TaskLabelRelation',
+        through_fields=('task', 'label')
+    )
+
     created_at = models.DateTimeField(auto_now_add=True)
+
+
+class TaskLabelRelation(models.Model):
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+
+    label = models.ForeignKey(Label, on_delete=models.PROTECT)
